0de4aa432e312441ce289d9c5b7e277c1b501b9a
Navid Pulikkottil <navidjp05@gmail.com>
2024-11-15 21:26:49 -0500

    fixed cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: "https://ru-autosnipe.vercel.app/",
+        origin: "https://ru-autosnipe.vercel.app",
     })
 );
 app.use("/api/users", usersRoutes);
