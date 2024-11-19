61d754f3d9b5278c829c1ed0b9998c1f3ec48b2a
Navid Pulikkottil <navidjp05@gmail.com>
2024-11-19 17:59:07 -0500

    attempt to fix problem with reading env variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,8 +23,6 @@ app.get("/api/open-courses", getCachedCourses);
 
 initializeProxy();
 
-console.log("Mongo URI:", process.env.MONGODB_URI);
-
 (async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, { dbName: "main" });
